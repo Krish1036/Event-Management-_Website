f69c7e43c388935451b1b5371ea568b40262fe91
KrishThakore <krshthakore@gmail.com>
2026-01-04 14:03:09 +0530

    new UI

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: D:\Event-Management\app\layout.tsx
+// File: C:\Japan\projects\Shared-event-management\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
